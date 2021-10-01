c3070052c4ff691c56faf5f59872dc630574c674
jennahorrall2 <jenna.horrall@libertymutual.com>
2021-10-01 10:18:56 -0700

    first commit

---
+++ dvd-library-backend/src/main/java/com/example/team15dvdlibrary/Team15DvdLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.team15dvdlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team15DvdLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team15DvdLibraryApplication.class, args);
+	}
+
+}
---
+++ dvd-library-backend/src/test/java/com/example/team15dvdlibrary/Team15DvdLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team15dvdlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team15DvdLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
